8e6c9b8ac8a9b4bbfd307d5e3b44e83822a2f0ee
SandrineLem <lemossandrine@gmail.com>
2022-02-17 14:41:15 +0100

    crud post ok

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -7,7 +7,7 @@ module.exports.readPost = (req, res) => {
   PostModel.find((err, docs) => {
     if (!err) res.send(docs);
     else console.log("Error to get data : " + err);
-  });
+  }).sort({ createdAt: -1 });
 };
 
 module.exports.createPost = async (req, res) => {
@@ -55,4 +55,148 @@ module.exports.deletePost = (req, res) => {
   });
 };
 
+module.exports.likePost = async (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID unknown : " + req.params.id);
 
+  try {
+    await PostModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $addToSet: { likers: req.body.id },
+      },
+      { new: true }
+    ).catch((err) => {
+      return res.status(400).send(err);
+    });
+
+    await UserModel.findByIdAndUpdate(
+      req.body.id,
+      {
+        $addToSet: { likes: req.params.id },
+      },
+      { new: true }
+    )
+      .then((docs) => {
+        return res.send(docs);
+      })
+      .catch((err) => {
+        return res.status(400).send(err);
+      });
+  } catch (err) {
+    return res.status(400).send(err);
+  }
+};
+
+module.exports.unlikePost = async (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID unknown : " + req.params.id);
+
+  try {
+    await PostModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $pull: { likers: req.body.id },
+      },
+      { new: true }
+    ).catch((err) => {
+      return res.status(400).send(err);
+    });
+
+    await UserModel.findByIdAndUpdate(
+      req.body.id,
+      {
+        $pull: { likes: req.params.id },
+      },
+      { new: true }
+    )
+      .then((docs) => {
+        return res.send(docs);
+      })
+      .catch((err) => {
+        return res.status(400).send(err);
+      });
+  } catch (err) {
+    return res.status(400).send(err);
+  }
+};
+
+module.exports.commentPost = (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID unknown : " + req.params.id);
+
+  try {
+    return PostModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $push: {
+          comments: {
+            commenterId: req.body.commenterId,
+            commenterPseudo: req.body.commenterPseudo,
+            text: req.body.text,
+            timestamp: new Date().getTime(),
+          },
+        },
+      },
+      { new: true }
+    )
+      .then((docs) => {
+        return res.send(docs);
+      })
+      .catch((err) => {
+        return res.status(400).send(err);
+      });
+  } catch (err) {
+    return res.status(400).send(err);
+  }
+};
+
+module.exports.editCommentPost = (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID unknown : " + req.params.id);
+
+  try {
+    return PostModel.findById(req.params.id, (err, docs) => {
+      const theComment = docs.comments.find((comment) =>
+        comment._id.equals(req.body.commentId)
+      );
+
+      if (!theComment) return res.status(404).send("Comment not found");
+      theComment.text = req.body.text;
+
+      return docs.save((err) => {
+        if (!err) return res.status(200).send(docs);
+        return res.status(500).send(err);
+      });
+    });
+  } catch (err) {
+    return res.status(400).send(err);
+  }
+};
+
+module.exports.deleteCommentPost = (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID unknown : " + req.params.id);
+
+  try {
+    return PostModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $pull: {
+          comments: {
+            _id: req.body.commentId,
+          },
+        },
+      },
+      { new: true }
+    )
+      .then((docs) => {
+        return res.send(docs);
+      })
+      .catch((err) => {
+        return res.status(400).send(err);
+      });
+  } catch (err) {
+    return res.status(400).send(err);
+  }
+};
--- models/post.model.js
+++ models/post.model.js
@@ -1,39 +1,41 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const PostSchema = new mongoose.Schema({
-  posterId: {
-    type: String,
-    required: true,
+const PostSchema = new mongoose.Schema(
+  {
+    posterId: {
+      type: String,
+      required: true
+    },
+    message: {
+      type: String,
+      trim: true,
+      maxlength: 500,
+    },
+    picture: {
+      type: String,
+    },
+    video: {
+      type: String,
+    },
+    likers: {
+      type: [String],
+      required: true,
+    },
+    comments: {
+      type: [
+        {
+          commenterId:String,
+          commenterPseudo: String,
+          text: String,
+          timestamp: Number,
+        }
+      ],
+      required: true,
+    },
   },
-  message: {
-    type: String,
-    trim: true,
-    maxlength: 500,
-  },
-  image: {
-    type: String,
-  },
-  video: {
-    type: String,
-  },
-  likers: {
-    type: [String],
-    requirer: true,
-  },
-  commments: {
-    type: [
-      {
-        commenterId: String,
-        commenterPseudo: String,
-        text: String,
-        timestamp: Number,
-      },
-    ],
-    required: true,
-  },
-},
-{
+  {
     timestamps: true,
-}
+  }
 );
+
 module.exports = mongoose.model('post', PostSchema);
