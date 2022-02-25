8104e944b16588e403dd55edaad836b06c97fe85
SandrineLem <lemossandrine@gmail.com>
2022-02-25 10:03:10 +0100

    authentification et message erreurs ok cote frontend avec backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { useState } from 'react';
 import Routes from "./components/Routes";
+import { UidContext } from './components/appContext';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 const App = () => {
+  const [uid,setUid] = useState(null);
+
+  useEffect(() => {
+    const fetchToken = async() => {
+      await axios( {
+      
+        method:"get",
+        url: `${process.env.REACT_APP_API_URL}jwtid`,
+        withCredentials:true
+      })
+      .then((res)=> setUid(res.data))
+      .catch((err) => console.log("No token"));
+    }
+    fetchToken();
+  }, []);
   return (
-    <div>
+    <UidContext.Provider value ={uid}>
     <Routes />
-    </div>
+    </UidContext.Provider>
   );
 };
 
--- client/src/pages/Profil.js
+++ client/src/pages/Profil.js
@@ -1,17 +1,26 @@
 import React from "react";
 import Log from "../components/Log";
+import { UidContext } from "../components/appContext";
+import { useContext } from "react";
 
 const Profil = () => {
+    const uid = useContext(UidContext);
+
     return (
        <div className="profil-page">
+       {uid ? (
+           <h1>UPDATE PAGE</h1>
+       ) : (
         <div className="logo-container">
             <Log signin={false} signup={true} />
             <div className="img-container">
             <img src="./img/log.svg" alt="img-log" />
             </div>
         </div>
+    )}
        </div>
     );
 };
+
 
 export default Profil;
