a26027b3568b5d2261d600923de79d3f5e437ea4
SandrineLem <lemossandrine@gmail.com>
2022-03-03 12:59:18 +0100

    fil actualite info post affichage ok

---
+++ client/src/components/Thread.js
@@ -0,0 +1,32 @@
+import React, {  useEffect, useState } from "react";
+import  { useDispatch, useSelector  } from 'react-redux';
+import { getPosts } from "../actions/post.action";
+import Card from "./Post/Card";
+import { isEmpty } from "./Utils";
+
+
+const Thread = () => {
+    const [loadPost, setLoadPost] = useState(true);
+    const dispatch = useDispatch();
+    const posts = useSelector((state) => state.postReducer);
+    useEffect(() => {
+        if (loadPost) {
+            dispatch(getPosts());
+            setLoadPost(false)
+        }
+    }, [loadPost, dispatch])
+
+    return (
+        <div className="thread-container">
+            <ul>
+                {!isEmpty(posts[0]) &&
+                    posts.map((post) => {
+                        return <Card post={post} key={post._id} />;
+                    })
+                }    
+            </ul>
+        </div>
+    );
+};
+
+export default Thread;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,16 @@
 import React from "react";
+import LeftNav from "../components/LeftNav";
+import Thread from "../components/Thread";
+
 
 const Home = () => {
     return (
-        <div>
-        Hello depuis Home
+       <div className="home">
+        <LeftNav />
+        <div className="main">
+            <Thread />
         </div>
+       </div>
     );
 };
 
